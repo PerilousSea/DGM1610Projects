8cd50b1e3e9eb7a4d05c65d01451ba712c58ee5d
Taykenbaken <10892594@uvu.edu>
2023-01-22 20:46:23 -0700

    1st Game hooray

---
+++ first game/Assets/Scripts/variables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    //[Access Modifier][Data-Type/ Type][Identifier/Name][Access Operator =][Value]
+
+    public string pokemon = "Squirtle";  
+    public int hp = 44;
+    private float height = 1.8f;
+    private double weight = 20.895d;
+    public char accessory = 'y';
+    public bool shiny = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
